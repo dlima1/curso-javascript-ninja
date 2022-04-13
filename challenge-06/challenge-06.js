310f06e77f1d3fbac304f724a478abd36e6652da
vertra-integrationsquade <80354526+vertra-integrationsquade@users.noreply.github.com>
2022-04-13 07:36:13 -0300

    desafio 06 resolvido

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship = 'Campeonato Pernambucano';
+console.log( championship )
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+teams = ['Náutico' , 'Sport', 'Santa Cruz', 'Salgueiro', 'Íbis']
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,19 +33,36 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition( teamPosition ){
+    teams = ['Náutico' , 'Sport', 'Santa Cruz', 'Salgueiro', 'Íbis']
+    var returnString = teamPosition <= 5 ?  "O time que está em " + teamPosition + "º lugar é o " + teams[ teamPosition-1 ] +'.' : 'Não temos a informação do time que está nessa posição.';
+    return(returnString)
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+var teamPosition = 1
+var positionReturned = showTeamPosition( teamPosition )
+console.log( positionReturned )
+
+var teamPosition = 2
+var positionReturned = showTeamPosition( teamPosition )
+console.log( positionReturned )
+
+var teamPosition = 7
+var positionReturned = showTeamPosition( teamPosition )
+console.log( positionReturned )
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+var x = 20
+while( x++ <= 30){
+    console.log(x-1)
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,9 +76,19 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
-
+function convertToHex( color ){
+    switch( color ){
+        case 'amarelo':
+            return( 'O hexadecimal da cor ' + color + ' é: ' + '#FFFF00' )
+        case 'vermelho':
+            return( 'O hexadecimal da cor ' + color + ' é: ' + '#FF0000' )
+        default:
+            return( 'Não temos o equivalente hexadecimal para ' + color )
+    }
+}
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+var color = 'verde'
+var returnHEX = convertToHex( color )
+console.log(returnHEX) 
