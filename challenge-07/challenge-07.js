18a399658a56975663a5ee313fa97c20c33d97f9
vertra-integrationsquade <80354526+vertra-integrationsquade@users.noreply.github.com>
2022-04-13 17:48:27 -0300

    modulos 7,8,9

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,51 +1,74 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+var array1 = [  function executeFunc( inputString){ 
+                    var prop1 = 'returned string: ' + inputString
+                    return (prop1)
+                } , 
+                'ola', 
+                { 
+                    prop1: 'do', 
+                    prop2: 're'
+                },
+                135.75,
+                1047896 
+            ]
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+var addItem = function addItem( inputArray , itemToAdd ){
+    inputArray.push(itemToAdd);
+    return(inputArray);
+};
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
-
+var arrayToAdd = [ 1 , 'ola', 355];
+console.log( addItem( array1 ,  arrayToAdd) )
+console.log(array1[0]( ' teste '))
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log( 'O segundo elemento do segundo array é ' + arrayToAdd[ arrayToAdd.length - 1 ])
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log( 'O primeiro array tem ' + array1.length + ' itens.')
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log( 'O primeiro array tem ' + arrayToAdd.length + ' itens.')
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
 console.log( 'Números pares entre 10 e 20:' );
-// ?
+var num = 10;
+while( num <= 20 ){
+    console.log( num%2 === 0 ? num : '' )
+    num++
+}
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
 console.log( 'Números ímpares entre 10 e 20:' );
-// ?
+num = 10;
+while( num <= 20 ){
+    console.log( num%2 !== 0 ? num : '' )
+    num++
+}
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -54,7 +77,13 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 console.log( 'Números pares entre 100 e 120:' );
-// ?
+num = 100
+for( num ; num <= 120 ; num++){
+    console.log( num%2 === 0 ? num : '' )
+}
 
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+num = 100
+for( num ; num <= 125 ; num++){
+    console.log( num%2 !== 0 ? num : '' )
+}
--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,26 +3,31 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum( a , b ){
+  return( a+b )
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+var a = 5, b = 9 ;
+console.log( "A soma de " + a + " e " + b + " é igual a " + sum(a , b) + ".")
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log("O nome da função que faz a soma é " + sum.name + ".")
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+var showName = function showName(){
+  return( showName.name )
+} 
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
@@ -34,7 +39,7 @@ Usando a variável criada acima, mostre no console o nome e o retorno da funçã
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log( "A função " + showName.name + " retorna " + showName() + "." )
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,25 +53,52 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+var calculator = function calculator( operation ){
+  return function( operatorA, operatorB){
+    switch( operation ){
+      case '+': 
+        return( operatorA + operatorB )
+      case '-':
+        return( operatorA - operatorB )
+      case '*':
+        return(operatorA * operatorB)
+      case '/':
+         return(operatorA / operatorB)
+      case '%':
+        return(operatorA % operatorB)
+      default:
+        return('operação inválida!')
+    }
+  } ;
+}
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
 parâmetro o operador de soma.
 */
-// ?
+var sum = calculator('+');
 
 /*
 Agora `sum` é uma função. Mostre no console a soma de dois números, usando ela.
 */
-// ?
+console.log( 'A soma de dois números é: ' + sum( 5, 5))
 
 /*
 Agora, declare algumas variáveis com os nomes `subtraction`, `multiplication`,
 `division` e `mod`, e atribua a elas a função `calculator`, passando o operador
 correto por parâmetro para cada uma delas.
 */
-// ?
+var subtraction = calculator('-');
+console.log( 'The subtraction of two numbers is: ' + subtraction( 5, 5))
+
+var multiplication = calculator('*');
+console.log( 'The multiplication of two numbers is: ' + multiplication( 5, 5))
+
+var division = calculator('/');
+console.log( 'The division of two numbers is: ' + division( 5, 5))
+
+var mod = calculator('%');
+console.log( 'The mod of two numbers is: ' + mod( 10, 4))
 
 /*
 Faça uma operação com cada uma das funções criadas acima, mostrando o resultado
